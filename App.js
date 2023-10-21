994fd35f9a4bd971dc29f26efa2d6010b0df9198
hsc03 <hsc0710@naver.com>
2023-10-21 19:48:55 +0900

    Change structure

--- App.js
+++ App.js
@@ -1,18 +1,28 @@
 import React from 'react';
-import Login from './screens/Login';
-import index from './screens/index';
+import SignInScreen from './screens/SignInScreen';
+import SignUpScreen from './screens/SignUpScreen';
+import Navigation from './screens/Navigation';
 import { createStackNavigator } from '@react-navigation/stack';
 import { NavigationContainer } from '@react-navigation/native';
 
+const Stack = createStackNavigator();
+
+function Auth() {
+  return (
+    <Stack.Navigator>
+      <Stack.Screen name="SignInScreen" component={SignInScreen} options={{ headerShown: false }} />
+      <Stack.Screen name="SignUpScreen" component={SignUpScreen} options={{ headerShown: false }} />
+    </Stack.Navigator>
+  );
+
+}
+
 export default function App() {
-
-  const Stack = createStackNavigator();
-
   return (
     <NavigationContainer>
-      <Stack.Navigator initialRoutName="Login">
-        <Stack.Screen name="Login" component={Login} options={{headerShown:false}}/>
-        <Stack.Screen name="index" component={index} options={{headerShown:false}}/>
+      <Stack.Navigator initialRoutName="SignInScreen">
+        <Stack.Screen name="Auth" component={Auth} options={{ headerShown: false }} />
+        <Stack.Screen name="Navigation" component={Navigation} options={{ headerShown: false }} />
       </Stack.Navigator>
     </NavigationContainer>
   );
